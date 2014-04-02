3435b4a776dcd528aa2dc648eb2eea4c567600ac
cjsolomon <cjs9135@AT227XWKS-003W.srunet.sruad.edu>
2014-04-02 08:50:25 -0400

    Revert "Update VehicleTruckForm.java"
    This reverts commit b5b7d468778209cd0440025e7b16f763629671ed.

--- MMRP/src/GUI/VehicleTruckForm.java
+++ MMRP/src/GUI/VehicleTruckForm.java
@@ -4,7 +4,6 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 import com.jgoodies.forms.factories.FormFactory;
@@ -12,24 +11,26 @@ import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
 
-import core.Plane;
+import core.Truck;
 import core.Vehicle;
 
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import javax.swing.JTabbedPane;
 
-public class VehiclePlaneForm extends JPanel {
+public class VehicleTruckForm extends JPanel {
 	JLabel contractorLabel,nameLabel,statusLabel;
 	JComboBox<String> contractorDropDown;
 	JTextField nameText;
 	VehicleSegmentTable vst;
 	//private Vehicle t;
-	private Plane p;
+	private Truck t;
 	private JTabbedPane tabbedPane;
 	private JPanel basic;
 	private JComboBox<String> status;
-	public VehiclePlaneForm()
+	public VehicleTruckForm()
 	{
 		String[] contractor = {Vehicle.Contractors.DHL.toString(),Vehicle.Contractors.FedEX.toString(),Vehicle.Contractors.UPS.toString(),Vehicle.Contractors.USPS.toString()};
 		setLayout(new FormLayout(new ColumnSpec[] {
@@ -93,12 +94,14 @@ public class VehiclePlaneForm extends JPanel {
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
-		
 		//add(contractorDropDown, "4, 20, left, top");
 		nameLabel = new JLabel("Name");
 		basic.add(nameLabel, "2, 2, left, center");
 		//nameText.setSize(69, Integer.parseInt(FormFactory.LINE_GAP_ROWSPEC.toString()));
 		//add(nameLabel,"2, 22, center, center");
+		nameText = new JTextField(20);
+		nameText.setText(setName());
+		basic.add(nameText, "4, 2, left, top");
 		contractorLabel = new JLabel();
 		contractorLabel.setText("Contractor");
 		basic.add(contractorLabel, "2, 4, left, center");
@@ -120,9 +123,6 @@ public class VehiclePlaneForm extends JPanel {
 				basic.add(statusLabel, "2, 6, left, center");
 			this.status=new JComboBox(Vehicle.Status.values());
 			basic.add(status, "4, 6, left, top");
-		nameText = new JTextField(20);
-		nameText.setText(setName());
-		basic.add(nameText, "4, 2, left, top");
 		this.setTypeForm();
 	}
 	private void loadVehicle(int id)
@@ -130,10 +130,10 @@ public class VehiclePlaneForm extends JPanel {
 		clearGUI();
 		if(id>0)
 		{
-			p=Plane.Load(id);
-			setPlane();
-		}
-		vst.ShowTable(p);
+			t=Truck.Load(id);
+			setTruck();
+		}			
+		vst.ShowTable(t);
 	}
 	public void showPanel()
 	{
@@ -160,23 +160,26 @@ public class VehiclePlaneForm extends JPanel {
 	{
 
 	}
-	private void setPlane()
+	private void setTruck()
 	{
-		this.contractorDropDown.setSelectedItem(Vehicle.loadContractor(p.getContractor()));
-		this.nameText.setText(p.getPlaneName());
+		this.contractorDropDown.setSelectedItem(Vehicle.loadContractor(t.getContractor()));
+		this.nameText.setText(t.getTruckName());
 	}
 	private String setName(){
 		int randomInt;
 		boolean goodNumber = false;
-		String name = "";
-		ArrayList<Plane> planes = new ArrayList<Plane>();
+		String name = "NEWTRUCK";
+		ArrayList<Truck> trucks = new ArrayList<Truck>();
 		int trycounter=0;
-		while(!goodNumber){
+		if(contractorDropDown!=null)
+		{
+		while(!goodNumber)
+		{
 			randomInt = (int)Math.floor(Math.random()*10000);
-			name = this.contractorDropDown.getSelectedItem().toString() + "Plane" + randomInt;
-			planes = Plane.LoadAll("Where PlaneName ='" +name + "'");
+			name = this.contractorDropDown.getSelectedItem().toString() + "Truck" + randomInt;
+			trucks = Truck.LoadAll("Where TruckName ='" +name + "'");
 			trycounter++;
-			if(!(planes.size() > 0)){
+			if(!(trucks.size() > 0)){
 				goodNumber = true;
 			}
 			else
@@ -187,6 +190,7 @@ public class VehiclePlaneForm extends JPanel {
 					goodNumber=true;
 				}
 			}
+		}
 		}
 		return name;
 	}
