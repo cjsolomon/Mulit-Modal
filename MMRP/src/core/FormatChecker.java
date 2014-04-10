dfc5852d062d015fce621ab6993d890e30a51254
zgp1001 <zgp1001@sru.edu>
2014-04-09 20:54:17 -0400

    Added log file outputs to Format Checker

--- MMRP/src/core/FormatChecker.java
+++ MMRP/src/core/FormatChecker.java
@@ -2,10 +2,15 @@ package core;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import GUI.Log;
+
 public class FormatChecker {
-	
-	/**TODO
-	 * Print out to log file 
+
+	/**
+	 * 
+	 * @param tel
+	 * @return True/False
+	 * Returns true if tel fits the required telephone/fax format: xxx-xxx-xxxx
 	 */
 	static public boolean isValidPhone(String tel)
 	{
@@ -16,10 +21,17 @@ public class FormatChecker {
 	    }
 	    else
 	    {
+	    	Log.writeLogWarning("Invalid format on phone/fax number.");
 	    	return false;
 	    }
 	}
 	
+	/**
+	 * 
+	 * @param address
+	 * @return True/False
+	 * Returns true if address fits the email address format: emailAddress@domain.com
+	 */
 	static public boolean isValidEmail(String address)
 	{
 		Pattern pattern = Pattern.compile("[\\d\\D]*@\\D*.[{com}{edu}{org}]");
@@ -29,10 +41,17 @@ public class FormatChecker {
 	    }
 	    else
 	    {
+	    	Log.writeLogWarning("Invalid format on email address.");
 	    	return false;
 	    }
 	}
 	
+	/**
+	 * 
+	 * @param area
+	 * @return True/False
+	 * Returns true if the area code fits the proper format: xxx
+	 */
 	static public boolean isValidAreaCode(String area)
 	{
 		Pattern pattern = Pattern.compile("\\d{3}");
@@ -42,10 +61,19 @@ public class FormatChecker {
 	    }
 	    else
 	    {
+	    	Log.writeLogWarning("Invalid format on area code.");
 	    	return false;
 	    }
 	}
 	
+	/**
+	 * 
+	 * @param check, the value to check if in range given
+	 * @param min, the minimum value
+	 * @param max, the maximum value
+	 * @return True/False
+	 * Returns true if check is inside of minimum and maximum values 
+	 */
 	static public boolean inRange(double check, double min, double max)
 	{
 		if(check >= min && check <= max) 
@@ -54,8 +82,31 @@ public class FormatChecker {
 		}
 		else 
 		{
+			Log.writeLogWarning("Invalid numerical entry given; not in range.");
 			return false;
 		}
+	}
+	
+	/**
+	 * 
+	 * @param day
+	 * @return True/False
+	 * Returns true if day is of the proper format: MonthName DayNumber Year 
+	 * ex) January 7th 2014
+	 */
+	static public boolean isValidDate(String day)
+	{
+		Pattern pattern = Pattern.compile("");
+	    Matcher matcher = pattern.matcher(day);
+	    if (matcher.matches()) {
+	    	return true;
+	    }
+	    else
+	    {
+	    	//Log.writeLogWarning("Invalid format on date.");
+	    	return false;
+	    }
+		
 	}
 
 }
