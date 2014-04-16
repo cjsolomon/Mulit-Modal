7a2fb71ebbf81af3ac9b6d2f7ef8bc1164e3712e
zgp1001 <zgp1001@sru.edu>
2014-04-16 11:59:10 -0400

    Email validation fix for literal '.' character and accepting whitespace

--- MMRP/src/core/FormatChecker.java
+++ MMRP/src/core/FormatChecker.java
@@ -41,8 +41,10 @@ public class FormatChecker {
 	 */
 	static public boolean isValidEmail(String address)
 	{
-		Pattern pattern = Pattern.compile("\\w\\w*@\\w\\w*\\.\\D\\D\\D[\\D]");
-		System.out.println("\\w\\w*@\\w\\w*\\.\\D\\D\\D[\\D]");
+		//address.replace('\\.', 'X');
+		//System.out.println(address);
+		Pattern pattern = Pattern.compile("\\S+@\\S+\\.\\S{3,4}");
+		//System.out.println("\\w+@\\w+.\\D\\D\\D[\\D]");
 	    Matcher matcher = pattern.matcher(address);
 	    if (matcher.matches()) {
 	    	return true;
