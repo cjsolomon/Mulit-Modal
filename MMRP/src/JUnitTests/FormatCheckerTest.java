fce185527008e346dad9df9a4b9f3bd5a20d8278
danMiller1983 <dan.v.miller@gmail.com>
2014-04-20 23:22:51 -0400

    FormatChecker JUnit Test
    1 Test Still remaining to write.  I believe that the code is still being
    written for it however.

--- MMRP/src/JUnitTests/FormatCheckerTest.java
+++ MMRP/src/JUnitTests/FormatCheckerTest.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 public class FormatCheckerTest {
 
 	@Test
-	public void isValidPhoneTest() {
+	public void testIsValidPhoneTest() {
 		String check_good[] = {"814-316-1530","999-999-9999","412-345-6789","724-856-4567"};
 		String check_bad[] = {"814-316-153","9999-999-9999","345-679","5856-4567","", "814 316 1444"};
 		
@@ -23,8 +23,8 @@ public class FormatCheckerTest {
 	}
 	
 	@Test
-	public void isValidEmailTest() {
-		String check_good[] = {"me@the.com","sam.thangiah@sru.edu","a@a.com","theMan@wooHoo.org"};
+	public void testIsValidEmailTest() {
+		String check_good[] = {"me@the.com","sam.thangiah@sru.edu","a@a.com","theMan@wooHoo.org", "isThisWorking@checkme.ca", "pleaseDontEmail@me.info"};
 		String check_bad[] = {"814-316-153","@jon.edu","theDanMiller@nothin","Marky mark@google.com",""};
 		
 		for (String check : check_good) {
@@ -43,4 +43,43 @@ public class FormatCheckerTest {
 			Assert.assertFalse(flag);
 		}
 	}
+	
+	@Test
+	public void testIsValidAreaCode() {
+		String check_good[] = {"000","123","456"};
+		String check_bad[] = {"814-316-153","@jon.edu","11 1","111.12",""};
+		
+		for (String check : check_good) {
+			boolean flag = FormatChecker.isValidAreaCode(check);
+			if (!flag) {
+				System.out.println("The following area code is returnig as invalid " + check);
+			}
+			Assert.assertTrue(flag);
+		}
+		for (String check : check_bad) {
+			boolean flag = FormatChecker.isValidAreaCode(check);
+			if (flag) {
+				System.out.println("The following area code is returning valid " + check);
+			}
+			Assert.assertFalse(flag);
+		}
+	}
+	
+	@Test
+	public void testInRange() {
+		Integer min = new Integer(7);
+		Integer max = new Integer(15);
+		
+		boolean flag = FormatChecker.inRange(min-1, min, max);
+		Assert.assertFalse(flag);
+		
+		for (int i = min; i <=max; i++) {
+			flag = FormatChecker.inRange(i, min, max);
+			Assert.assertTrue("This should report a number in range", flag);
+		}
+		
+		flag = FormatChecker.inRange(max+1, min, max);
+		Assert.assertFalse(flag);
+	}
+	
 }
