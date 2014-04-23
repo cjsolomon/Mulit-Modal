722ba41f38eef8dff59f51880601e6e24b2eb22b
danMiller1983 <dan.v.miller@gmail.com>
2014-04-22 21:54:34 -0400

    need jordans help - something wrong

--- MMRP/src/JUnitTests/SegmentTest.java
+++ MMRP/src/JUnitTests/SegmentTest.java
@@ -2,6 +2,7 @@ package JUnitTests;
 
 import core.Segment;
 import core.Vehicle;
+import core.ShippingRate;
 
 import org.junit.*;
 
@@ -12,7 +13,16 @@ public class SegmentTest {
 		Segment test_segment = new Segment();
 		Assert.assertEquals(new Integer(Segment.getDefaultEndLocationId()), new Integer(test_segment.getEndLocationID()));
 		Assert.assertEquals(new Integer(Segment.getDefaultStartLocationId()), new Integer(test_segment.getStartLocationID()));
-		Assert.assertEquals(Vehicle.TravelModes.TRUCK, test_segment.getVehicle().getTravelMode());
+		Assert.assertEquals(Vehicle.TravelModes.TRUCK.toString(), test_segment.getVehicle().getTravelMode());
+		Assert.assertEquals(new Double(Segment.getDefaultDistance()), new Double(test_segment.getDistance()));
+		Assert.assertEquals(new Integer(Segment.getDefaultDepartureTime()), new Integer(test_segment.getEstimatedDepartureTime()));
+		Assert.assertEquals(new Integer(Segment.getDefaultArrivalTime()), new Integer(test_segment.getEstimatedArrivalTime()));
+		Assert.assertEquals(new Integer(Segment.getDefaultEarliestDepartureTime()), new Integer(test_segment.getEarliestDepartureTime()));
+		Assert.assertEquals(new Integer(Segment.getDefaultEarliestArrivalTime()), new Integer(test_segment.getEarliestArrivalTime()));
+		Assert.assertEquals(new Integer(Segment.getDefaultLatestDepartureTime()), new Integer(test_segment.getLatestDepartureTime()));
+		Assert.assertEquals(new Integer(Segment.getDefaultLatestArrivalTime()), new Integer(test_segment.getLatestArrivalTime()));
+		Assert.assertEquals(new ShippingRate().getType().toString(), test_segment.getShippingRate().getType().toString());
+		
 		
 	}
 }
