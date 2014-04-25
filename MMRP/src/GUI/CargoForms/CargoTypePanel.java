58379478585263b9ad3cdf605197bc6fea8c0a48
JordanSchiller1064 <jms1064@sru.edu>
2014-04-25 11:56:28 -0500

    Uhh more Gui, not pushing all at once for some reason

---
+++ MMRP/src/GUI/CargoForms/CargoTypePanel.java
@@ -0,0 +1,74 @@
+package GUI.CargoForms;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JButton;
+
+public class CargoTypePanel extends JPanel {
+	CargoTypeTable ctt;
+	JScrollPane sp;
+	public CargoTypePanel()
+	{
+		sp = new JScrollPane();
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(74dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(85dlu;default)"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("max(147dlu;default)"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("max(15dlu;default)"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("max(15dlu;default)"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("max(15dlu;default)"),}));
+		ctt = new CargoTypeTable();
+		ctt.setVisible(false);
+		sp.setViewportView(ctt);
+		sp.setVisible(false);
+		add(sp,"2, 2, 21, 12");
+		
+		JButton btnNewButton = new JButton("New button");
+		add(btnNewButton, "22, 14");
+		
+	}
+	public void showPanel()
+	{
+		sp.setVisible(true);
+		ctt.showPanel();
+		sp.setViewportView(ctt);
+		this.setVisible(true);
+	}
+
+}
